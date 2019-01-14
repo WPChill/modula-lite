1fc89115ed29c5ba0c0eeaf3c4ce65b551bf00ce
George Ciobanu <george.ciobanu91@gmail.com>
2019-01-14 22:30:05 +0200

    removed unnecessary files

--- includes/admin/tabs/addons.php
+++
@@ -1,6 +0,0 @@
-<div class="wrap">
-	<h1 class="wp-heading-inline"><?php esc_html_e( 'Modula Addons', 'modula-best-grid-gallery' ) ?></h1>
-	<a href="#" id="modula-force-reload" class="page-title-action"><?php esc_html_e( 'Reload Addons', 'modula-best-grid-gallery' ) ?></a>
-	<p><?php esc_html_e( 'Addons are used to extend the core functionality of Modula. You can automatically install an addon from this page if available.', 'modula-best-grid-gallery' ) ?></p>
-</div>
-<div class="modula-addons-error"></div>
