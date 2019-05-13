698f561dfae0cdf254fefbd3edf24b894470df71
razvanaldea89 <razvan.aldea89@yahoo.ro>
2019-05-13 08:21:56 +0300

    added extra filter for lightboxes

--- includes/public/class-modula-shortcode.php
+++ includes/public/class-modula-shortcode.php
@@ -29,7 +29,8 @@ class Modula_Shortcode {
 		wp_register_style( 'modula', MODULA_URL . 'assets/css/modula.css', null, MODULA_LITE_VERSION );
 
 		// Scripts necessary for some galleries
-		wp_register_script( 'lightbox2_script', MODULA_URL . 'assets/js/lightbox.min.js', array( 'jquery' ), MODULA_LITE_VERSION, true );
+		/*@TODO modified from .min*/
+		wp_register_script( 'lightbox2_script', MODULA_URL . 'assets/js/lightbox.js', array( 'jquery' ), MODULA_LITE_VERSION, true );
 		wp_register_script( 'packery', MODULA_URL . 'assets/js/packery.min.js', array( 'jquery' ), MODULA_LITE_VERSION, true );
 		wp_register_script( 'lazysizes', MODULA_URL . 'assets/js/lazysizes.min.js', array( 'jquery' ), MODULA_LITE_VERSION, true );
 
