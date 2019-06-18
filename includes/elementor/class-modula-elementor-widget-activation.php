84b641243c6f9061d14c2c2879cdd0128d8b90cd
razvanaldea89 <razvan.aldea89@yahoo.ro>
2019-06-18 18:19:14 +0300

    added action to enqueue needed styles and scripts to elementor previe ~ still needs some work

--- includes/elementor/class-modula-elementor-widget-activation.php
+++ includes/elementor/class-modula-elementor-widget-activation.php
@@ -39,6 +39,33 @@ class Modula_Elementor_Widget_Activation {
 
 		// Register widgets
 		add_action( 'elementor/widgets/widgets_registered', array( $this, 'register_widgets' ) );
+
+		// Enqueue needed scripts and styles in Elementor preview
+		add_action( 'elementor/preview/enqueue_scripts', array( $this, 'modula_elementor_enqueue_scripts' ) );
+
+		add_action( 'elementor/preview/enqueue_styles', array( $this, 'modula_elementor_enqueue_styles' ) );
+	}
+
+	public function test( $settings ) {
+		return $settings;
+	}
+
+
+	/**
+	 * Enqueue scripts in Elementor preview
+	 */
+	public function modula_elementor_enqueue_scripts() {
+		wp_enqueue_script( 'modula', MODULA_URL . 'assets/js/jquery-modula.min.js', array( 'jquery' ), MODULA_LITE_VERSION, true );
+		// @TODO enqueue script from pro vesion
+		//wp_enqueue_script( 'modula-pro', '/wp-content/plugins/modula-pro/assets/js/modula-pro.js', array( 'jquery' ), '', true );
+	}
+
+	/**
+	 *  Enqueue styles in Elementor preview
+	 */
+	public function modula_elementor_enqueue_styles(){
+		// @TODO enqueue style from pro version
+		//wp_enqueue_style( 'modula-pro-effects', MODULA_PRO_PATH.'assets/css/effects.css', '', null );
 	}
 }
 
