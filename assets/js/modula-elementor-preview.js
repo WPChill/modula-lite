7bccc2d1c942c19148140bf3f3a55a7a520ecf5c
razvanaldea89 <razvan.aldea89@yahoo.ro>
2019-06-19 20:10:38 +0300

    function to trigger modula update when changing gallery from elementor widget

--- assets/js/modula-elementor-preview.js
+++ assets/js/modula-elementor-preview.js
@@ -1,5 +1,11 @@
 jQuery(function($){
-    elementorFrontend.hooks.addAction( 'frontend/element_ready/widget', function( $scope ) {
-        $(document).trigger('modula-update');
+    elementor.hooks.addAction( 'panel/open_editor/widget/modula_elementor_gallery', function( panel, model, view ) {
+        var gallery_select = panel.$el.find('select[data-setting="modula_gallery_select"]');
+        gallery_select.on('change',function(){
+            var option = $(this).find('option:selected');
+            if('none' != option.val()){
+                $(document).trigger('modula-update');
+            }
+        });
     } );
 });
