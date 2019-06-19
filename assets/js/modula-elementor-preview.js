c5c5be8c8ba03cddc56205a7a0e26bf0294f995e
razvanaldea89 <razvan.aldea89@yahoo.ro>
2019-06-19 20:29:44 +0300

    formating only

--- assets/js/modula-elementor-preview.js
+++ assets/js/modula-elementor-preview.js
@@ -1,11 +1,12 @@
-jQuery(function($){
-    elementor.hooks.addAction( 'panel/open_editor/widget/modula_elementor_gallery', function( panel, model, view ) {
+jQuery(function ($) {
+    elementor.hooks.addAction('panel/open_editor/widget/modula_elementor_gallery', function (panel, model, view) {
         var gallery_select = panel.$el.find('select[data-setting="modula_gallery_select"]');
-        gallery_select.on('change',function(){
+        gallery_select.on('change', function () {
             var option = $(this).find('option:selected');
-            if('none' != option.val()){
+            if ('none' != option.val()) {
                 $(document).trigger('modula-update');
+                console.log(option.val());
             }
         });
-    } );
+    });
 });
