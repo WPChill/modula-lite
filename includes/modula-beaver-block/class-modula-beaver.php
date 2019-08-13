ca0abcc775d5a7808db1eb52da5c56a61e279e78
razvanaldea89 <razvan.aldea89@yahoo.ro>
2019-08-13 10:46:52 +0300

    changed file name / added scripts for preview only on selected beaver post types /

--- includes/modula-beaver-block/class-modula-beaver.php
+++ includes/modula-beaver-block/class-modula-beaver.php
@@ -6,13 +6,45 @@ if (!defined('ABSPATH')) {
 
 class Modula_Beaver {
 
+    /**
+     * Modula_Beaver constructor.
+     */
     public function __construct() {
         add_action('init', array($this, 'include_beaver_block'));
+        add_action('admin_enqueue_scripts', array($this, 'enqueue_page_builder_scripts'));
+
     }
 
+    /**
+     * Include Modula Beaver Block
+     */
     public function include_beaver_block() {
         if (class_exists('FLBuilder')) {
-            require_once 'modula-beaver-block.php';
+            require_once 'class-modula-beaver-block.php';
+        }
+    }
+
+    /**
+     * Enqueue needed scripts in the admin required for pagebuilder preview
+     */
+    public function enqueue_page_builder_scripts() {
+
+        // only enqueue for SiteOrigin page builder
+        if (class_exists('FLBuilder')) {
+
+            // get Beaver Builder post types
+            $beaver_post_types = FLBuilderModel::get_post_types();
+            $current_screen    = get_current_screen();
+
+            if (in_array($current_screen->post_type, $beaver_post_types)) {
+                wp_register_style('modula', MODULA_URL . 'assets/css/modula.min.css', null, MODULA_LITE_VERSION);
+                wp_register_script('modula-preview', MODULA_URL . 'assets/js/jquery-modula.min.js', array('jquery'), MODULA_LITE_VERSION, true);
+                wp_register_script('modula-beaver-preview', MODULA_URL . 'assets/js/modula-beaver-preview.js', array('jquery'), MODULA_LITE_VERSION, true);
+
+                wp_enqueue_style('modula');
+                wp_enqueue_script('modula-preview');
+                wp_enqueue_script('modula-beaver-preview');
+            }
         }
     }
 }
