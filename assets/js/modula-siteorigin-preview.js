0753cceddc0ef955da80c0ef7788cd9746a265d3
razvanaldea89 <razvan.aldea89@yahoo.ro>
2019-08-14 10:41:00 +0300

    siteorigin builder preview update

--- assets/js/modula-siteorigin-preview.js
+++ assets/js/modula-siteorigin-preview.js
@@ -1,12 +1,15 @@
 jQuery(function ($) {
     $(window).load(function () {
+        // set timeout to let the editor preview load completely
         setTimeout(function () {
             gallery_preview();
             block_preview_image_click();
         }, 600);
-        siteorigin_modula_gallery_change();
     });
 
+    /**
+     * Reinitiate the Modula Gallery when editing the post and nonlive preview
+     */
     function gallery_preview() {
 
         var modula_gallery = $('.modula-gallery');
@@ -26,34 +29,12 @@ jQuery(function ($) {
         }
     }
 
-    function block_preview_image_click(){
-        $('.modula-gallery a.tile-inner').click(function(e){
+    /**
+     * Prevent lightbox openinng when
+     */
+    function block_preview_image_click() {
+        $('.modula-gallery a.tile-inner').click(function (e) {
             e.preventDefault();
         });
     }
-
-    function siteorigin_modula_gallery_change() {
-
-        $('#modula_widget_gallery_select').on('change', function () {
-
-            alert('changed');
-            var modula_gallery = $('.modula-gallery');
-
-            if (modula_gallery.length > 0) {
-                var galleryID = modula_gallery.attr('id'),
-                    modulaSettings = modula_gallery.data('config'),
-                    modulaInstance = jQuery('#' + galleryID).data('plugin_modulaGallery');
-
-                if (modulaInstance) {
-                    modulaInstance.destroy();
-                    galleryID = $('#modula_widget_gallery_select').val();
-                    jQuery('#' + galleryID).data('plugin_modulaGallery', null);
-                }
-
-                $('#' + galleryID).modulaGallery(modulaSettings);
-
-            }
-        });
-    }
-
 });
