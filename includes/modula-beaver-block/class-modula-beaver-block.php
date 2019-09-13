9c8940ebc8874513a995799aae61c8c21b8ffbfb
George Ciobanu <george.ciobanu91@gmail.com>
2019-09-13 11:00:17 +0300

    change class verification

--- includes/modula-beaver-block/class-modula-beaver-block.php
+++ includes/modula-beaver-block/class-modula-beaver-block.php
@@ -8,9 +8,9 @@ class Modula_Beaver_Block extends FLBuilderModule {
 
     public function __construct() {
         parent::__construct(array(
-            'name'            => __('Modula Gallery', 'modula-best-grid-gallery'),
-            'description'     => __('A block for Modula Gallery', 'modula-best-grid-gallery'),
-            'category'        => __('Modula', 'modula-best-grid-gallery'),
+            'name'            => __( 'Modula Gallery', 'modula-best-grid-gallery' ),
+            'description'     => __( 'A block for Modula Gallery', 'modula-best-grid-gallery' ),
+            'category'        => __( 'Modula', 'modula-best-grid-gallery' ),
             'icon'            => 'format-image.svg',
             'dir'             => MODULA_PATH . 'includes/modula-beaver-block/',
             'url'             => MODULA_URL . 'includes/modula-beaver-block/',
@@ -21,14 +21,14 @@ class Modula_Beaver_Block extends FLBuilderModule {
 
 FLBuilder::register_module('Modula_Beaver_Block', array(
     'modula_gallery' => array(
-        'title'    => __('Modula Gallery', 'modula-best-grid-gallery'),
+        'title'    => __( 'Modula Gallery', 'modula-best-grid-gallery' ),
         'sections' => array(
             'modula_gallery_section' => array(
-                'title'  => __('Select the Modula Gallery you want', 'modula-best-grid-gallery'),
+                'title'  => __( 'Select the Modula Gallery you want', 'modula-best-grid-gallery' ),
                 'fields' => array(
                     'modula_gallery_select' => array(
                         'type'    => 'select',
-                        'label'   => __('Select Modula Gallery', 'modula-best-grid-gallery'),
+                        'label'   => __( 'Select Modula Gallery', 'modula-best-grid-gallery' ),
                         'default' => 'none',
                         'options' => Modula_Helper::get_galleries(),
                     )
--- includes/modula-beaver-block/includes/frontend.php
+++ includes/modula-beaver-block/includes/frontend.php
@@ -8,5 +8,5 @@ $gallery_id = $settings->modula_gallery_select;
 if ('none' != $gallery_id) {
     echo do_shortcode('[Modula id="' . $gallery_id . '"]');
 } else {
-    echo esc_html__('No gallery was selected', 'modula-best-grid-gallery');
+    echo esc_html__( 'No gallery was selected', 'modula-best-grid-gallery' );
 }
