03cf12c54e4a8c40192cb8b7aa26e5485c0e474f
George Ciobanu <george.ciobanu91@gmail.com>
2019-09-13 11:00:17 +0300

    change class verification

--- includes/modula-beaver-block/class-modula-beaver.php
+++ includes/modula-beaver-block/class-modula-beaver.php
@@ -30,11 +30,11 @@ class Modula_Beaver {
     public function enqueue_page_builder_scripts() {
 
         // only enqueue for Beaver page builder live editing
-        if (class_exists('FLBuilderModule') && FLBuilderModel::is_builder_active()) {
+        if (class_exists('FLBuilderModel') && FLBuilderModel::is_builder_active()) {
 
             wp_register_script('modula-beaver-preview', MODULA_URL . 'assets/js/modula-beaver-preview.js', array('jquery'), MODULA_LITE_VERSION, true);
-
             wp_enqueue_script('modula-beaver-preview');
+            
         }
     }
 }
