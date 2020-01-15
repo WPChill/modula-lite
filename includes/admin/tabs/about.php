eec6d6aa612915d1281926baf8a1988cd4b0367d
razvanaldea89 <razvan.aldea89@yahoo.ro>
2020-01-15 08:01:41 +0200

    #267 about page style update; bug/feature presentation update /

--- includes/admin/tabs/about.php
+++ includes/admin/tabs/about.php
@@ -1,17 +1,41 @@
+<?php
+$issues  = array(
+    'fix'     => array(
+        'Fix max-width issue with Twenty Twenty theme'
+    ),
+    'feature' => array(
+        'Added Uninstall options to remove data entries from DB',
+        'Added troubleshooting options to enqueue CSS and JS files everywhere',
+        'Added support for WebP files'
+    )
+);
+?>
 <div class="row modula-about-row">
-    <div class="modula-about-header">
-        <h1 class="wp-clearfix modula-about-heading"><?php esc_html_e('What\'s new in version ' . MODULA_LITE_VERSION . ' of Modula ', ''); ?></h1>
-    </div>
-    <div class="modula-about-content">
-        <h3>Version <?php echo MODULA_LITE_VERSION; ?> comes with a bunch updates, including:</h3>
-        <ul class="modula-about-list">
-            <li class="feature">Added Uninstall options to remove data entries from DB</li>
-            <li class="feature">Added troubleshooting options to enqueue CSS and JS files everywhere</li>
-            <li class="fix">Fix max-width issue with Twenty Twenty theme</li>
-            <li class="feature">Added support for WebP files</li>
-        </ul>
-    </div>
-    <div class="modula-about-footer">
-        <p>Presented to you by the friendly folks over at <a href="https://machothemes.com" target="_blank">MachoThemes</a> !</p>
+    <div class="about__container">
+        <div class="about__header modula-about-header">
+            <div class="about__header-title modula-about-heading">
+                <h1>Modula<span><?php echo MODULA_LITE_VERSION; ?></span></h1>
+            </div>
+            <div class="about__header-badge"></div>
+            <div class="about__header-text">
+                <p><?php esc_html_e('Modula is the most powerful, user-friendly WordPress gallery plugin. Add galleries, masonry grids and more in a few clicks.','modula-best-grid-gallery'); ?></p>
+            </div>
+        </div>
+        <div class="modula-about-content">
+            <?php if (!empty($issues)) { ?>
+
+                <h2><?php printf(esc_html__('Version %s addressed %s bug and implemented %s features.', 'modula-best-grid-gallery'), MODULA_LITE_VERSION, count($issues['fix']), count($issues['feature'])); ?></h2>
+                <ul class="modula-about-list">
+                    <?php
+                    foreach ($issues as $key => $iss) {
+                        foreach ($iss as $is) {
+                            echo "<li class='$key'>$is</li>";
+                        }
+                    }
+                    ?>
+                </ul>
+
+            <?php } ?>
+        </div>
     </div>
 </div>
