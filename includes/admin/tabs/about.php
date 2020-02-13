77433fdb91e97ca658c319567adf689d1399adc6
George Ciobanu <george.ciobanu91@gmail.com>
2020-02-13 12:26:58 +0200

    version bump / about page

--- includes/admin/tabs/about.php
+++ includes/admin/tabs/about.php
@@ -1,24 +1,9 @@
 <?php
 $issues = array(
-    'feature' => array(
-        esc_html__( 'Added Uninstall options to remove data entries from DB', 'modula-best-grid-gallery' ),
-        esc_html__( 'Added troubleshooting options to enqueue CSS and JS files everywhere', 'modula-best-grid-gallery' ),
-        esc_html__( 'Added support for WebP files', 'modula-best-grid-gallery' ),
-        esc_html__( 'Added WhatsApp as social icon', 'modula-best-grid-gallery' ),
-        esc_html__( 'Added numbers to hover effects', 'modula-best-grid-gallery' ),
-        esc_html__( 'Added cursor controls', 'modula-best-grid-gallery' ),
-        esc_html__( 'Added social icons size and gutter', 'modula-best-grid-gallery' )
-    ),
     'fix'     => array(
-        esc_html__( 'Fix max-width issue with Twenty Twenty theme', 'modula-best-grid-gallery' ),
-        esc_html__( 'Fix menu entry colouring bug', 'modula-best-grid-gallery' ),
-        esc_html__( 'Fix modula-item background', 'modula-best-grid-gallery' ),
-        esc_html__( 'Re-worded "Update" button', 'modula-best-grid-gallery' ),
-        esc_html__( 'Fixed copy shortcode button design bug', 'modula-best-grid-gallery' )
+        esc_html__( 'Fixed Captions', 'modula-best-grid-gallery' ),
+        esc_html__( 'Fixed security issue', 'modula-best-grid-gallery' ),
     ),
-    'removal' => array(
-        esc_html__( 'Removed settings for default title and caption', 'modula-best-grid-gallery' )
-    )
 );
 ?>
 <div class="row modula-about-row">
@@ -35,7 +20,7 @@ $issues = array(
         <div class="modula-about-content">
             <?php if (!empty($issues)) { ?>
 
-                <h2><?php printf(esc_html__('Version %s addressed %s bug and implemented %s features.', 'modula-best-grid-gallery'), MODULA_LITE_VERSION, count($issues['fix']), count($issues['feature'])); ?></h2>
+                <h2><?php printf(esc_html__('Version %s addressed %s bugs', 'modula-best-grid-gallery'), MODULA_LITE_VERSION, count( $issues['fix'] ) ); ?></h2>
                 <ul class="modula-about-list">
                     <?php
                     foreach ($issues as $key => $iss) {
