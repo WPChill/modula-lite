5110369ef189cad6005cdf551768080ef817b80f
razvanaldea89 <razvan.aldea89@yahoo.ro>
2020-02-27 11:12:29 +0200

    #121 #257  fixed clearfix; fixed single item preview positioning; update test button positioning;

--- includes/admin/class-modula-field-builder.php
+++ includes/admin/class-modula-field-builder.php
@@ -415,8 +415,7 @@ class Modula_Field_Builder {
 				
 
 				$html .= '</select>';
-				$html .= '<p class="description">' . esc_html__( 'Select an hover effect', 'modula-best-grid-gallery' ) . '</p>';
-
+                $html .= '<p class="description">' . esc_html__( 'Select an hover effect', 'modula-best-grid-gallery' ) . '</p>';
                 $html .= '<div class="effect-compatibility">';
                 $html .= '<p class="description">' . esc_html__( 'This effect is compatible with:', 'modula-best-grid-gallery' );
                 $html .= '<span><strong> ' . esc_html__( 'Title', 'modula-best-grid-gallery' ) . '</strong></span>,';
@@ -526,18 +525,18 @@ class Modula_Field_Builder {
         foreach ( $hovers as $key => $name ) {
 
             if ( 'none' == $key ) {
-                $effect .= '<div class="panel panel-' . esc_attr( $key ) . ' modula-items clearfix"></div>';
+                $effect .= '<div class="panel panel-' . esc_attr( $key ) . ' modula-items wp-clearfix"></div>';
             } elseif ( 'pufrobo' == $key ) {
                 if ( !is_array( $image ) ) {
                     // Pufrobo Effect
-                    $effect .= '<div class="panel panel-pufrobo modula-items clearfix">';
+                    $effect .= '<div class="panel panel-pufrobo modula-items wp-clearfix">';
                     $effect .= '<div class="modula-item effect-pufrobo">' . $image . '<div class="figc"><div class="figc-inner"><h2>Lorem ipsum</h2><p class="description">Quisque diam erat, mollisvitae enim eget</p><div class="jtg-social"><a class="fa fa-twitter" href="#">' . Modula_Helper::get_icon( 'twitter' ) . '</a><a class="fa fa-facebook" href="#">' . Modula_Helper::get_icon( 'facebook' ) . '</a><a class="fa fa-pinterest" href="#">' . Modula_Helper::get_icon( 'pinterest' ) . '</a><a class="fa fa-whatsapp" href="#">' . Modula_Helper::get_icon( 'whatsapp' ) . '</a><a class="fa fa-linkedin" href="#">' . Modula_Helper::get_icon( 'linkedin' ) . '</a></div></div></div></div>';
                     $effect .= '</div>';
                 } else {
                     $effect .= '<div class="modula-hover-preview-slider">';
                     foreach ( $image as $i ) {
                         // Pufrobo Effect
-                        $effect .= '<div class="panel panel-pufrobo modula-items clearfix">';
+                        $effect .= '<div class="panel panel-pufrobo modula-items wp-clearfix">';
                         $effect .= '<div class="modula-item effect-pufrobo">' . $i . '<div class="figc"><div class="figc-inner"><h2>Lorem ipsum</h2><p class="description">Quisque diam erat, mollisvitae enim eget</p><div class="jtg-social"><a class="fa fa-twitter" href="#">' . Modula_Helper::get_icon( 'twitter' ) . '</a><a class="fa fa-facebook" href="#">' . Modula_Helper::get_icon( 'facebook' ) . '</a><a class="fa fa-pinterest" href="#">' . Modula_Helper::get_icon( 'pinterest' ) . '</a><a class="fa fa-whatsapp" href="#">' . Modula_Helper::get_icon( 'whatsapp' ) . '</a><a class="fa fa-linkedin" href="#">' . Modula_Helper::get_icon( 'linkedin' ) . '</a></div></div></div></div>';
                         $effect .= '</div>';
                     }
@@ -576,8 +575,9 @@ class Modula_Field_Builder {
         }
 
         $html .= '<div class="modula-scaling-effect-preview modula-effects-preview modula">';
-        $html .= '<div class="panel panel-none modula-items clearfix"></div>';
-        $html .= '<div class="panel modula-items clearfix">';
+        $html .= '<a href="#" class="button button-primary" id="test-scaling-preview">' . esc_html__( 'Test effect', 'modula-best-grid-gallery' ) . '</a>';
+        $html .= '<div class="panel panel-none modula-items wp-clearfix"></div>';
+        $html .= '<div class="panel modula-items wp-clearfix">';
         for ( $i = 0; $i <= 3; $i++ ) {
 
             $preview_image = isset( $image[ $i ] ) ? $image[ $i ] : '<img src="' . MODULA_URL . '/assets/images/effect.jpg" class="pic">';
@@ -585,7 +585,7 @@ class Modula_Field_Builder {
         }
 
         $html .= '</div>';
-        $html .= '<a href="#" class="button button-primary" id="test-scaling-preview">' . esc_html__( 'Test effect', 'modula-best-grid-gallery' ) . '</a>';
+        $html .= '<div class="wp-clearfix"></div>';
         $html .= '</div>';
 
         return $html;
