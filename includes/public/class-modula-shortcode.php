0a20a846bc4d4c35de9fde0adcfbe5d2e91d383c
AndylukAK <alexandru.tirim@yahoo.com>
2020-03-02 10:45:58 +0200

    https://github.com/MachoThemes/modula-pro/issues/231
     The problem was in modula-lite. It works now . Needs testing .

--- includes/public/class-modula-shortcode.php
+++ includes/public/class-modula-shortcode.php
@@ -162,7 +162,7 @@ class Modula_Shortcode {
 			// Gallery container attributes
 			'gallery_container' => array(
 				'id' => $gallery_id,
-				'class' => array( 'modula', 'modula-gallery' ),
+				'class' => apply_filters( 'modula_gallery_extra_classes', 'modula modula-gallery', $data->settings ),
 			),
 
 			// Items container attributes
