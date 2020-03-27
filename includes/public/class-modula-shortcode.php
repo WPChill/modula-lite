ea6a9f6936ed393ed911dea302683637d0565e3b
AndylukAK <alexandru.tirim@yahoo.com>
2020-03-27 16:56:03 +0200

    Version Bump / Update changelog
    Removed filter

--- includes/public/class-modula-shortcode.php
+++ includes/public/class-modula-shortcode.php
@@ -162,7 +162,7 @@ class Modula_Shortcode {
 			// Gallery container attributes
 			'gallery_container' => array(
 				'id' => $gallery_id,
-				'class' => apply_filters( 'modula_gallery_extra_classes', 'modula modula-gallery', $settings ),
+				'class' => array( 'modula', 'modula-gallery' ),
 			),
 
 			// Items container attributes
