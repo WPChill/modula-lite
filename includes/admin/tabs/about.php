ac404abc61eddbbb237b9804ab060e4e913d5084
razvanaldea89 <44024114+razvanaldea89@users.noreply.github.com>
2020-06-09 11:16:27 +0300

    update about page

--- includes/admin/tabs/about.php
+++ includes/admin/tabs/about.php
@@ -1,11 +1,14 @@
 <?php
 $issues = array(
     'fix'     => array(
-        esc_html__( 'Fixed Fancybox always opening, not depending on lightbox & links type', 'modula-best-grid-gallery' ),
-        esc_html__( 'Fixed lazyload for masonry columns', 'modula-best-grid-gallery' ),
-        esc_html__( 'Fixed layout rebuild on device orientation change', 'modula-best-grid-gallery' ),
+        esc_html__( 'Importing Envira Galleries image size, custom dimensions and gutter.', 'modula-best-grid-gallery' ),
+        esc_html__( 'Extensions menu entry always last.', 'modula-best-grid-gallery' ),
+        esc_html__( 'Social icons are now disabled by default when creating a new gallery.', 'modula-best-grid-gallery' ),
     ),
     'feature' => array(
+	    esc_html__( 'Added autosuggest URL to image URL field.', 'modula-best-grid-gallery' ),
+	    esc_html__( 'Added share via Email.', 'modula-best-grid-gallery' ),
+	    esc_html__( 'Added "Save gallery"/"Update gallery" shortcut CTRL/CMD + S', 'modula-best-grid-gallery' ),
     )
 );
 ?>
@@ -22,7 +25,7 @@ $issues = array(
         <div class="modula-about-content">
             <?php if (!empty($issues)) { ?>
 
-                <h2><?php printf(esc_html__('Version %s addressed %s bugs.', 'modula-best-grid-gallery'), MODULA_LITE_VERSION, count( $issues['fix'] )); ?></h2>
+                <h2><?php printf(esc_html__('Version %s addressed %s bugs and implemented %s features.', 'modula-best-grid-gallery'), MODULA_LITE_VERSION, count( $issues['fix']) ,count($issues['feature'])); ?></h2>
                 <ul class="modula-about-list">
                     <?php
                     foreach ($issues as $key => $iss) {
