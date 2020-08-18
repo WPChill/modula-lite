582ca765e785d94f8bca19de30047fb7aa32f0b7
razvanaldea89 <44024114+razvanaldea89@users.noreply.github.com>
2020-08-18 13:37:34 +0300

    #430  elementor compatibility fix ~ master; version bump; changelog update;

--- assets/js/admin/modula-elementor-preview.js
+++ assets/js/admin/modula-elementor-preview.js
@@ -1,22 +1,21 @@
-jQuery(function ($) {
+jQuery( function ( $ ) {
+	jQuery( window ).on( 'elementor/frontend/init', () => {
 
-    elementorFrontend.hooks.addAction( 'frontend/element_ready/modula_elementor_gallery.default', function( $scope ) {
+		elementorFrontend.hooks.addAction( 'frontend/element_ready/modula_elementor_gallery.default', function ( $scope ) {
+			var $gallery = $scope.find( '.modula-gallery' );
 
-        var $gallery = $scope.find( '.modula-gallery' );
+			if ( $gallery.length > 0 ) {
+				var galleryID      = $gallery.attr( 'id' ),
+				    modulaSettings = $gallery.data( 'config' ),
+				    modulaInstance = jQuery( '#' + galleryID ).data( 'plugin_modulaGallery' );
 
-        if ( $gallery.length > 0 ) {
-            var galleryID = $gallery.attr( 'id' ),
-                modulaSettings = $gallery.data( 'config' ),
-                modulaInstance = jQuery( '#' + galleryID ).data( 'plugin_modulaGallery' );
-                
-            if ( modulaInstance ) {
-                modulaInstance.destroy();
-                jQuery( '#' + galleryID ).data( 'plugin_modulaGallery', null );
-            }
+				if ( modulaInstance ) {
+					modulaInstance.destroy();
+					jQuery( '#' + galleryID ).data( 'plugin_modulaGallery', null );
+				}
 
-            $( '#' + galleryID ).modulaGallery( modulaSettings );
-        }
-
-    } );
-
-});
+				jQuery( '#' + galleryID ).modulaGallery( modulaSettings );
+			}
+		} );
+	} );
+} );
