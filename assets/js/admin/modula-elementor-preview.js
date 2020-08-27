66ebbae1afc9cf48355905cec2bd85a770f765b2
George Ciobanu <george.ciobanu91@gmail.com>
2020-08-27 10:34:00 +0300

    elementor widget / version bump / changelog

--- assets/js/admin/modula-elementor-preview.js
+++ assets/js/admin/modula-elementor-preview.js
@@ -1,26 +1,6 @@
 jQuery( function ( $ ) {
 
-	if ( 'undefined' == typeof jQuery.migrateVersion ) {
-		jQuery( window ).on( 'elementor/frontend/init', () => {
-
-			elementorFrontend.hooks.addAction( 'frontend/element_ready/modula_elementor_gallery.default', function ( $scope ) {
-				var $gallery = $scope.find( '.modula-gallery' );
-
-				if ( $gallery.length > 0 ) {
-					var galleryID      = $gallery.attr( 'id' ),
-					    modulaSettings = $gallery.data( 'config' ),
-					    modulaInstance = jQuery( '#' + galleryID ).data( 'plugin_modulaGallery' );
-
-					if ( modulaInstance ) {
-						modulaInstance.destroy();
-						jQuery( '#' + galleryID ).data( 'plugin_modulaGallery', null );
-					}
-
-					jQuery( '#' + galleryID ).modulaGallery( modulaSettings );
-				}
-			} );
-		} );
-	} else {
+	if ( 'undefined' != typeof elementorFrontend.hooks ) {
 		elementorFrontend.hooks.addAction( 'frontend/element_ready/modula_elementor_gallery.default', function ( $scope ) {
 			var $gallery = $scope.find( '.modula-gallery' );
 
@@ -36,6 +16,29 @@ jQuery( function ( $ ) {
 
 				jQuery( '#' + galleryID ).modulaGallery( modulaSettings );
 			}
-		} );
+		});
 	}
+
+
+	jQuery( window ).on( 'elementor/frontend/init', function(){
+
+		elementorFrontend.hooks.addAction( 'frontend/element_ready/modula_elementor_gallery.default', function ( $scope ) {
+			var $gallery = $scope.find( '.modula-gallery' );
+
+			if ( $gallery.length > 0 ) {
+				var galleryID      = $gallery.attr( 'id' ),
+				    modulaSettings = $gallery.data( 'config' ),
+				    modulaInstance = jQuery( '#' + galleryID ).data( 'plugin_modulaGallery' );
+
+				if ( modulaInstance ) {
+					modulaInstance.destroy();
+					jQuery( '#' + galleryID ).data( 'plugin_modulaGallery', null );
+				}
+
+				jQuery( '#' + galleryID ).modulaGallery( modulaSettings );
+			}
+		});
+
+	});	
+
 } );
