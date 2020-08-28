9e9c42cdac61172ad18b68d4a4b2ef1f1cbe3b08
razvanaldea89 <44024114+razvanaldea89@users.noreply.github.com>
2020-08-28 13:06:14 +0300

    react-dom import and dom query

--- includes/divi-extension/includes/modules/ModulaDivi/ModulaDivi.jsx
+++ includes/divi-extension/includes/modules/ModulaDivi/ModulaDivi.jsx
@@ -1,6 +1,7 @@
 // External Dependencies
 import React, {Component} from 'react';
 import $ from 'jquery';
+import { findDOMNode } from 'react-dom';
 
 // Internal Dependencies
 import './style.css';
@@ -11,24 +12,33 @@ class ModulaDivi extends Component {
 	static slug = 'modula_gallery';
 
 	render () {
-
 		return (
 			<div dangerouslySetInnerHTML={{ __html: this.props.modula_images }}></div>
 		);
 	}
 
 	componentDidMount () {
+		this._setup_modula();
+	}
 
-		var modulaGalleries = $( '.modula.modula-gallery' );
+	_setup_modula(){
+		const grid = findDOMNode(this.refs.grid);
+		console.log($(findDOMNode(this)));
+		if (!grid) return;
 
-		$.each( modulaGalleries, function () {
+		/*if(this.masonry){
+			this.masonry.masonry('destroy');
+		}
 
-			var modulaID       = $( this ).attr( 'id' ),
-			    modulaSettings = $( this ).data( 'config' );
+		this.masonry = $(grid).dss_masonry_gallery();
 
-			$( '#' + modulaID ).modulaGallery( modulaSettings );
+		this.resizeObserver = new ResizeObserver(entries => {
+			this.masonry.masonry('layout');
+		});
 
-		} );
+		this.resizeObserver.observe(grid);
+		this._fix_overlay_icon();*/
+		//console.log("MasonryGallery._setup_masonry:  end", this.props.moduleInfo.address);
 	}
 
 }
