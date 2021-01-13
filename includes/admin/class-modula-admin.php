c79d5974d4ad91a0ad522d03002b8beddc658a49
razvanaldea89 <44024114+razvanaldea89@users.noreply.github.com>
2021-01-13 16:22:04 +0200

    import export headers and padding

--- includes/admin/class-modula-admin.php
+++ includes/admin/class-modula-admin.php
@@ -381,16 +381,20 @@ class Modula_Admin {
      */
     public function import_export_doc() {
         ?>
-        <div class="wrap">
-            <h3><?php esc_html_e('Import Galleries','modula-best-grid-gallery'); ?></h3>
-            <p><?php esc_html_e('In order to import exported galleries head over to "Tools -> Import" or click','modula-best-grid-gallery'); ?> <a href="<?php echo admin_url('import.php'); ?>"><?php  esc_html_e('here.','modula-best-grid-gallery'); ?></a></p>
-            <p><?php echo '<a href="'.esc_url('https://wordpress.org/plugins/wordpress-importer/').'" target="_blank">'.esc_html__('Install Wordpress Importer','modula-best-grid-gallery').'</a>'.esc_html__('( if not installed ). If installed, click on Wordpress "Run importer". After that select the export file you desire and click "Upload file and import".','modula-best-grid-gallery'); ?></p>
-        </div>
-        <br />
-        <h3><?php esc_html_e('Export Galleries','modula-best-grid-gallery'); ?></h3>
-        <p><?php esc_html_e('In order to export Modula galleries head over to "Tools -> Export" or click','modula-best-grid-gallery'); ?> <a href="<?php echo admin_url('export.php'); ?>"><?php  esc_html_e('here.','modula-best-grid-gallery'); ?></a></p>
-        <p><?php echo esc_html__('Select "Galleries" and click "Download Export File". An export file will be created and downloaded, which will be used to import the galleries somewhere else.','modula-best-grid-gallery'); ?></p>
-        </div>
+			<?php Modula_Admin_Helpers::modula_page_header(); ?>
+		<div class="wrap">
+			<h3><?php esc_html_e( 'Import Galleries', 'modula-best-grid-gallery' ); ?></h3>
+			<p><?php esc_html_e( 'In order to import exported galleries head over to "Tools -> Import" or click', 'modula-best-grid-gallery' ); ?>
+				<a href="<?php echo admin_url( 'import.php' ); ?>"><?php esc_html_e( 'here.', 'modula-best-grid-gallery' ); ?></a>
+			</p>
+			<p><?php echo '<a href="' . esc_url( 'https://wordpress.org/plugins/wordpress-importer/' ) . '" target="_blank">' . esc_html__( 'Install Wordpress Importer', 'modula-best-grid-gallery' ) . '</a>' . esc_html__( '( if not installed ). If installed, click on Wordpress "Run importer". After that select the export file you desire and click "Upload file and import".', 'modula-best-grid-gallery' ); ?></p>
+			<br/>
+			<h3><?php esc_html_e( 'Export Galleries', 'modula-best-grid-gallery' ); ?></h3>
+			<p><?php esc_html_e( 'In order to export Modula galleries head over to "Tools -> Export" or click', 'modula-best-grid-gallery' ); ?>
+				<a href="<?php echo admin_url( 'export.php' ); ?>"><?php esc_html_e( 'here.', 'modula-best-grid-gallery' ); ?></a>
+			</p>
+			<p><?php echo esc_html__( 'Select "Galleries" and click "Download Export File". An export file will be created and downloaded, which will be used to import the galleries somewhere else.', 'modula-best-grid-gallery' ); ?></p>
+		</div>
         <?php
 
     }
