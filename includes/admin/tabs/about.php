bd1cdb2d316976e537fd140e74296eb140d36118
razvanaldea89 <44024114+razvanaldea89@users.noreply.github.com>
2021-04-05 10:51:27 +0300

    version bump; changelog update; about page update;

--- includes/admin/tabs/about.php
+++ includes/admin/tabs/about.php
@@ -1,34 +1,17 @@
 <?php
 $issues = array(
-	'fixed'     => array(
-		esc_html( 'Powered by links appear 2 times', 'modula-best-grid-gallery' ),
-		esc_html__( 'Gutenberg alignment issue when trying to make full width', 'modula-best-grid-gallery' ),
-		esc_html__( 'Elementor compatibility', 'modula-best-grid-gallery' ),
-		esc_html__( 'Selecting a gallery with no images in Gutenberg', 'modula-best-grid-gallery' ),
-		esc_html__( 'Modula\'s galleries not being displayed properly in preview in Gutenberg', 'modula-best-grid-gallery' ),
-		esc_html__( 'Modula\'s gallery selector not being displayed properly in it\'s Gutenberg\'s block', 'modula-best-grid-gallery' ),
-		esc_html__( 'Avada theme compatibility issue regarding color pickers', 'modula-best-grid-gallery' ),
-		esc_html__( 'Multiple same gallery in page issue', 'modula-best-grid-gallery' ),
-		esc_html__( 'JS error in admin when cycling through gallery\'s images', 'modula-best-grid-gallery' ),
-		esc_html__( 'Lazy load incompatibility with Site Ground Optimizer plugin and Avada lazy loading', 'modula-best-grid-gallery' ),
-		esc_html__( 'Conflict where ResizeSensor was declared as global. Now it has been personalized ', 'modula-best-grid-gallery' ),
-		esc_html__( 'Incompatibility with some themes, where the resize reset of Modula wasn\'t working correctly', 'modula-best-grid-gallery' ),
-		esc_html__( 'Modula\'s instance not being reset on tab switch', 'modula-best-grid-gallery' ),
-		esc_html__( 'Custom CSS\'s tab editor was not showing correctly if the last tab was the Custom CSS tab', 'modula-best-grid-gallery' ),
-	),
-	'added' => array(
-			esc_html__('Only allow certain users to the Extensions page','modula-best-grid-gallery'),
-			esc_html__('Responsive gutters','modula-best-grid-gallery'),
-			esc_html__('Added debug info using WordPress\' Site Health + added an export option to export single galleries, used for both debugging and export/import operations','modula-best-grid-gallery'),
-			esc_html__('FREE vs Premium page','modula-best-grid-gallery'),
-			esc_html__('Srcset and sizes for galleries images for both declared sizes and custom sizes','modula-best-grid-gallery'),
-			esc_html__('Custom size and WordPress image sizes selection for galleries grid thumbnail','modula-best-grid-gallery'),
-	),
-	'changed' => array(
-			esc_html__('Social share now gives image URL instead of page URL and title/caption of image','modula-best-grid-gallery'),
-			esc_html__('Extensions page and how it works','modula-best-grid-gallery'),
-			esc_html__('Hover effects tab had been given an UI update','modula-best-grid-gallery'),
-	)
+		'added'   => array(),
+		'changed' => array(),
+		'fixed'   => array(
+				esc_html__( 'Gutter for mobile and tablet after Modula update from 2.4.1 to 2.5.0. Setting was displaying OK in backend but on frontend change wasn\'t displayed correctly.', 'modula-best-grid-gallery' ),
+				esc_html__( 'Backwards compatibility for custom grid image size after Modula update from 2.4.1 to 2.5.0. Setting showed same value although in frontend the setting worked.', 'modula-best-grid-gallery' ),
+		)
+);
+
+$status = array(
+		'fixed'   => esc_html__( 'Fixed', 'modula-best-grid-gallery' ),
+		'added'   => esc_html__( 'Added', 'modula-best-grid-gallery' ),
+		'changed' => esc_html__( 'Changed', 'modula-best-grid-gallery' ),
 );
 
 ?>
@@ -48,12 +31,12 @@ $issues = array(
             <?php if (!empty($issues)) { ?>
             <ul class="modula-about-list">
                 <?php
-                foreach ($issues as $key => $iss) {
-                    foreach ($iss as $is) {
-                        echo "<li class='$key'>$is</li>";
-                    }
-                }
-                ?>
+				foreach ( $issues as $key => $iss ) {
+					foreach ( $iss as $is ) {
+						echo "<li class='$key'>$status[$key]: $is</li>";
+					}
+				}
+				?>
             </ul>
 
             <?php } ?>
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: best gallery plugin, image gallery, video gallery, free gallery, wordpress
 Requires at least: 5.2
 Tested up to: 5.7
 Requires PHP: 5.6
-Stable tag: 2.5.0
+Stable tag: 2.5.1
 License: GNU General Public License v3.0 or later
 
 The WordPress gallery plugin that's highlighy customizable & you can use to impress your clients. Create beautiful image galleries in minutes.
