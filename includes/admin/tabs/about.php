d3948732e155246fc7b19cf3dcc7e3423da7b200
razvanaldea89 <44024114+razvanaldea89@users.noreply.github.com>
2022-02-18 14:01:12 +0200

    changelog update; about page update;

--- includes/admin/tabs/about.php
+++ includes/admin/tabs/about.php
@@ -1,12 +1,38 @@
 <?php
 $issues = array(
-	'added'   => array(),
+	'added'   => array(
+        esc_html__( 'Link to Modula\'s about page in the plugin\'s branding header', 'modula-best-grid-gallery' ),
+        esc_html__( 'Upsell to Modula PRO in plugins page', 'modula-best-grid-gallery' ),
+        esc_html__( 'Mobile Gallery Height setting', 'modula-best-grid-gallery' ),
+    ),
 	'changed' => array(
-        esc_html__( 'Albums upsell' ),
-        esc_html__( 'Free vs Premium page and Upsells integration' ),
+        esc_html__( 'Gallery title html element (from h2 to div)', 'modula-best-grid-gallery' ),
+        esc_html__( 'Default "Hide Title" setting value to ON', 'modula-best-grid-gallery' ),
+        esc_html__( 'Lazy load setting default ON', 'modula-best-grid-gallery' ),
+        esc_html__( 'Updated CPT settings conditions', 'modula-best-grid-gallery' ),
+        esc_html__( 'Grid Automatic default Row Height from 150 to 250', 'modula-best-grid-gallery' ),
+        esc_html__( 'Update settings texts.', 'modula-best-grid-gallery' ),
+        esc_html__( 'Last 5 galleries now appear in selectize without searching', 'modula-best-grid-gallery' ),
+        esc_html__( 'Import/Export page received a new design', 'modula-best-grid-gallery' ),
+        esc_html__( 'Improved Upsells', 'modula-best-grid-gallery' ),
     ),
 	'fixed'   => array(
-		esc_html__( 'Added data-srcset and data-sizes attributes for lazy load functionality', 'modula-best-grid-gallery' ),
+        esc_html__( 'Modula metabox return to default position if previously moved into the sidebar and further prevent dragging the metabox.' ),
+        esc_html__( 'Modula gallery display in tabs/accordions', 'modula-best-grid-gallery' ),
+        esc_html__( 'Get proper mime type', 'modula-best-grid-gallery' ),
+        esc_html__( 'Added tracking db options to uninstall process', 'modula-best-grid-gallery' ),
+        esc_html__( 'Unset link image attribute when importing from NextGEN', 'modula-best-grid-gallery' ),
+        esc_html__( 'Error when trying to get images that were not imported correctly / do not exist as entries in db', 'modula-best-grid-gallery' ),
+        esc_html__( 'Incompatibility with Gutenberg block and widgets', 'modula-best-grid-gallery' ),
+        esc_html__( 'Modula Widget before and after args', 'modula-best-grid-gallery' ),
+        esc_html__( 'Sharing on LinkedIn', 'modula-best-grid-gallery' ),
+        esc_html__( 'Migration from NextGEN galleries replacement for `[nggallery id=”xx″]` shortcode format', 'modula-best-grid-gallery' ),
+        esc_html__( 'JavaScript error when Syntax Highlighting is disabled', 'modula-best-grid-gallery' ),
+        esc_html__( 'Don\'t enqueue scripts/styles when not needed', 'modula-best-grid-gallery' ),
+        esc_html__( 'Mobile/table gutter not working correctly', 'modula-best-grid-gallery' ),
+        esc_html__( 'Set a default widht of 100% when there is no value', 'modula-best-grid-gallery' ),
+        esc_html__( 'Admin Notice placement in Modula\'s Settings page', 'modula-best-grid-gallery' ),
+        esc_html__( 'Compatibility issue with Meow Lightbox', 'modula-best-grid-gallery' ),
 	)
 );
 
