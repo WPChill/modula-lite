6b9f6d39d1c62c33a75a7795579f7163aef1f5d1
andylukak <alexandru.tirim@yahoo.com>
2022-03-16 16:11:27 +0200

    #640
    Accesibility strings translatable also

--- includes/public/modula-helper-functions.php
+++ includes/public/modula-helper-functions.php
@@ -104,8 +104,8 @@ function modula_check_lightboxes_and_links( $item_data, $item, $settings ) {
 
 		$item_data['link_attributes']['class'][]    = 'modula-simple-link';
 		$item_data['item_classes'][]                = 'modula-simple-link';
-		$item_data['link_attributes']['aria-label'] = 'Open attachment page';
-		$item_data['link_attributes']['title']      = 'Open attachment page';
+		$item_data['link_attributes']['aria-label'] = esc_html__('Open attachment page', 'modula-best-grid-gallery');
+		$item_data['link_attributes']['title']      = esc_html__('Open attachment page', 'modula-best-grid-gallery');
 		if ( '' != $item['link'] ) {
 
 			$item_data['link_attributes']['href'] = $item['link'];
@@ -124,16 +124,16 @@ function modula_check_lightboxes_and_links( $item_data, $item, $settings ) {
 		$item_data['link_attributes']['href']       = $item_data['image_full'];
 		$item_data['link_attributes']['class'][]    = 'modula-simple-link';
 		$item_data['item_classes'][]                = 'modula-simple-link';
-		$item_data['link_attributes']['aria-label'] = 'Open image';
-		$item_data['link_attributes']['title']      = 'Open image';
+		$item_data['link_attributes']['aria-label'] = esc_html__('Open image', 'modula-best-grid-gallery');
+		$item_data['link_attributes']['title']      = esc_html__('Open image', 'modula-best-grid-gallery');
 
 	} else {
 
 		$item_data['link_attributes']['href']          = $item_data['image_full'];
 		$item_data['link_attributes']['rel']           = $settings['gallery_id'];
 		$item_data['link_attributes']['data-caption']  = $caption;
-		$item_data['link_attributes']['aria-label']    = 'Open image in lightbox';
-		$item_data['link_attributes']['title']         = 'Open image in lightbox';
+		$item_data['link_attributes']['aria-label']    = esc_html__('Open image in lightbox', 'modula-best-grid-gallery');
+		$item_data['link_attributes']['title']         = esc_html__('Open image in lightbox', 'modula-best-grid-gallery');
 
 	}
 
