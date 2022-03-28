b59d8183c4c9cd64b2d725bafd2f5c4e27d523ad
razvanaldea89 <44024114+razvanaldea89@users.noreply.github.com>
2022-03-28 07:53:26 +0300

    #648 add final tiles migrator

--- includes/admin/class-modula-addons.php
+++ includes/admin/class-modula-addons.php
@@ -37,13 +37,13 @@ class Modula_Addons {
 				'name' => __( 'Migrate away from PhotoBlocks', 'modula-best-grid-gallery' ),
 				'image' => 'https://wp-modula.com/wp-content/uploads/edd/2021/04/069-refresh.png',
 				'description'   => esc_html__('Want to change your gallery plugin and impress your potential clients with a fully customizable WordPress gallery plugin that\'s fully mobile responsive', 'moudla-best-grid-gallery')
-			),
+			),*/
 			'modula-final-tiles-migrator' => array(
 				'slug' => 'modula-final-tiles-migrator',
 				'name' => __( 'Migrate away from Final Tiles', 'modula-best-grid-gallery' ),
 				'image' => 'https://wp-modula.com/wp-content/uploads/edd/2021/04/069-refresh.png',
 				'description'   => esc_html__('Want to change your gallery plugin and impress your potential clients with a fully customizable WordPress gallery plugin that\'s fully mobile responsive', 'moudla-best-grid-gallery')
-			) */
+			)
 		) );
 	}
 
