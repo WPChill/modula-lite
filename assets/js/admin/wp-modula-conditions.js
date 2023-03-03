4669da7972830182d0c5ebf93982616892033ff3
Razvan Aldea <44024114+razvanaldea89@users.noreply.github.com>
2023-03-03 15:06:19 +0200

    Update wp-modula-conditions.js

--- assets/js/admin/wp-modula-conditions.js
+++ assets/js/admin/wp-modula-conditions.js
@@ -136,12 +136,12 @@ var modulaGalleryConditions = Backbone.Model.extend({
 		if ( 'fancybox' == value ) {
 
 			rows.filter('[data-container="show_navigation"]').setting_state( this, 'on');
-			tabs.filter('[data-tab="modula-exif"],[data-tab="modula-zoom"]').hide();
+			tabs.filter('[data-tab="modula-exif"],[data-tab="modula-zoom"]').show();
 
 		} else {
 
 			rows.filter('[data-container="show_navigation"]').setting_state( this, 'off');
-			tabs.filter('[data-tab="modula-exif"],[data-tab="modula-zoom"]').show();
+			tabs.filter('[data-tab="modula-exif"],[data-tab="modula-zoom"]').hide();
 
 		}
 
