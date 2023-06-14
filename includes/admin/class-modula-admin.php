998c8ae6d7b035852fac17777a7d3a1e3d8395c1
TeoAlex <teoalexsin@gmail.com>
2023-06-14 18:09:01 +0300

    [ #789 ] security issue

--- includes/admin/class-modula-admin.php
+++ includes/admin/class-modula-admin.php
@@ -473,6 +473,13 @@ class Modula_Admin {
 			wp_send_json( array( 'status' => 'failed' ) );
 		}
 
+		$current_user = wp_get_current_user();
+		$ptype = get_post_type_object( 'modula-gallery' );
+
+		if ( ! current_user_can( $ptype->cap->edit_others_posts ) && absint( get_post_field( 'post_author', $gallery_id ) ) !== absint( $current_user->ID ) ) {
+			wp_send_json( array( 'status' => __( 'Sorry, you do not have enough permissions.' ) ) );
+		}
+
 		if ( ! isset( $_POST['image'] ) ) {
 			wp_send_json( array( 'status' => 'failed' ) );
 		}
