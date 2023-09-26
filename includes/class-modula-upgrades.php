61d7e72ea3e6f81d0fc47a1931a187bfffe1f779
Raiber Cristian <raibercristian@gmail.com>
2023-09-26 19:46:35 +0300

    Formatting

--- includes/class-modula-upgrades.php
+++ includes/class-modula-upgrades.php
@@ -10,7 +10,7 @@ class Modula_Upgrades {
 	private $completed_upgrades = array();
 
 	function __construct() {
-		$upgrades = array(
+		$upgrades       = array(
 			'modula_v2' => array(
 				'notice'   => esc_html__( 'Modula needs to upgrade the database, click %shere%s to start the upgrade.', 'modula-best-grid-gallery' ),
 				'id'       => 'modula-upgrade-v2',
@@ -33,6 +33,7 @@ class Modula_Upgrades {
 		if ( ! $inst ) {
 			$inst = new Modula_Upgrades();
 		}
+
 		return $inst;
 	}
 
@@ -52,26 +53,26 @@ class Modula_Upgrades {
 	}
 
 	public function check_on_activate() {
-		
+
 		// Check if is a new 2.0.0 install or an old install
-        $version       = get_option( 'modula_version', array() );
-		$first_install =  empty( $version );
+		$version       = get_option( 'modula_version', array() );
+		$first_install = empty( $version );
 		$upgrade       = false;
 
-		if(!empty($version) && $version['current_version'] !== MODULA_LITE_VERSION ){
-		    $upgrade = true;
-        }
+		if ( ! empty( $version ) && $version['current_version'] !== MODULA_LITE_VERSION ) {
+			$upgrade = true;
+		}
 
 		if ( empty( $version ) ) {
 			if ( ! $this->check_upgrade_complete( 'modula_v2' ) && $this->check_old_db() ) {
-				$version['upgraded_from'] = '1.3.1';
+				$version['upgraded_from']   = '1.3.1';
 				$version['current_version'] = MODULA_LITE_VERSION;
-			}else{
-				$version['upgraded_from'] = MODULA_LITE_VERSION;
+			} else {
+				$version['upgraded_from']   = MODULA_LITE_VERSION;
 				$version['current_version'] = MODULA_LITE_VERSION;
 			}
-		}else{
-			$version['upgraded_from'] = $version['current_version'];
+		} else {
+			$version['upgraded_from']   = $version['current_version'];
 			$version['current_version'] = MODULA_LITE_VERSION;
 		}
 
@@ -84,9 +85,9 @@ class Modula_Upgrades {
 	/**
 	 * Display Upgrade Notices
 	 *
-	 * @since 2.0.0
 	 * @return void
-	*/
+	 * @since 2.0.0
+	 */
 	public function show_upgrade_notices() {
 
 		$version = get_option( 'modula_version' );
@@ -112,15 +113,17 @@ class Modula_Upgrades {
 	/**
 	 * Create Pages for each upgrades
 	 *
-	 * @since 2.0.0
 	 * @return void
-	*/
+	 * @since 2.0.0
+	 */
 	public function add_subpages() {
 		foreach ( $this->upgrades as $key => $upgrade ) {
-			add_submenu_page( '', $upgrade['title'], $upgrade['title'], 'manage_options', $upgrade['id'], array( $this, $upgrade['callback'] ) );
+			add_submenu_page( '', $upgrade['title'], $upgrade['title'], 'manage_options', $upgrade['id'], array(
+				$this,
+				$upgrade['callback']
+			) );
 		}
 	}
-
 
 
 	/* Helper Functions */
@@ -136,9 +139,9 @@ class Modula_Upgrades {
 	private function check_old_db() {
 
 		global $wpdb;
-		$table_name = $wpdb->prefix.'modula';
-		if( $wpdb->get_var( "SHOW TABLES LIKE '$table_name'" ) != $table_name ) {
-		    return false;
+		$table_name = $wpdb->prefix . 'modula';
+		if ( $wpdb->get_var( "SHOW TABLES LIKE '$table_name'" ) != $table_name ) {
+			return false;
 		} else {
 			return true;
 		}
@@ -149,10 +152,10 @@ class Modula_Upgrades {
 		if ( 'admin_page_modula-upgrade-v2' == $hook ) {
 			wp_enqueue_script( 'modula-upgrade', MODULA_URL . 'assets/js/admin/modula-upgrade.js', array( 'jquery' ), '2.0.0', true );
 			$args = array(
-				'ajaxurl'                 => admin_url( 'admin-ajax.php' ),
-				'get_galleries_nonce'     => wp_create_nonce( 'modula-get-galleries-nonce' ),
-				'upgrade_gallery_nonce'   => wp_create_nonce( 'modula-upgrade-gallery-nonce' ),
-				'upgrade_complete_nonce'  => wp_create_nonce( 'modula-upgrade-complete-nonce' ),
+				'ajaxurl'                => admin_url( 'admin-ajax.php' ),
+				'get_galleries_nonce'    => wp_create_nonce( 'modula-get-galleries-nonce' ),
+				'upgrade_gallery_nonce'  => wp_create_nonce( 'modula-upgrade-gallery-nonce' ),
+				'upgrade_complete_nonce' => wp_create_nonce( 'modula-upgrade-complete-nonce' ),
 			);
 			wp_localize_script( 'modula-upgrade', 'modulaUpgraderHelper', $args );
 		}
@@ -162,8 +165,8 @@ class Modula_Upgrades {
 	public function modula_upgrade_v2() {
 
 		global $wpdb;
-		$table_name = $wpdb->prefix.'modula';
-		$galleries = $wpdb->get_var( "SELECT COUNT(Id) as galleries FROM $table_name" );
+		$table_name = $wpdb->prefix . 'modula';
+		$galleries  = $wpdb->get_var( "SELECT COUNT(Id) as galleries FROM $table_name" );
 
 		if ( '0' == $galleries ) {
 
@@ -172,7 +175,7 @@ class Modula_Upgrades {
 			echo '<a href="' . esc_url( admin_url( 'post-new.php?post_type=modula-gallery' ) ) . '" class="button button-primary button-hero">' . esc_html__( 'Create a gallery now !', 'modula-best-grid-gallery' ) . '</a>';
 			echo '</div>';
 
-		}else{
+		} else {
 
 			$tab = isset( $_GET['tab'] ) ? sanitize_text_field( wp_unslash( $_GET['tab'] ) ) : 'import-all';
 
@@ -195,7 +198,7 @@ class Modula_Upgrades {
 			if ( 'import-all' == $tab ) {
 				echo '<p>' . esc_html__( 'This will import all your galleries.', 'modula-best-grid-gallery' ) . '</p>';
 				echo '<a href="#" id="modula-upgrade-v2" class="button button-primary">' . esc_html__( 'Start upgrade', 'modula-best-grid-gallery' ) . '</a>';
-			}elseif ( 'custom-import' == $tab ) {
+			} elseif ( 'custom-import' == $tab ) {
 
 				global $wpdb;
 				$galleries_query = 'SELECT * FROM ' . $wpdb->prefix . 'modula';
@@ -255,12 +258,12 @@ class Modula_Upgrades {
 
 		// Check if we already have imported this gallery
 		$post_args = array(
-			'post_type' => 'modula-gallery',
+			'post_type'   => 'modula-gallery',
 			'post_status' => 'publish',
-			'meta_query' => array(
+			'meta_query'  => array(
 				array(
-					'key'     => 'modula-id',
-					'value'   => $gallery_ID,
+					'key'   => 'modula-id',
+					'value' => $gallery_ID,
 				),
 			),
 		);
@@ -282,15 +285,15 @@ class Modula_Upgrades {
 
 		if ( $gallery ) {
 
-			$id = $gallery->Id;
+			$id     = $gallery->Id;
 			$config = json_decode( $gallery->configuration, true );
 
 			$images_query = "SELECT * FROM {$wpdb->prefix}modula_images WHERE gid={$id} ORDER BY sortOrder";
-			$images = $wpdb->get_results( $images_query, ARRAY_A );
+			$images       = $wpdb->get_results( $images_query, ARRAY_A );
 
 			// Insert the gallery post
 			$galery_data = array(
-				'post_type' => 'modula-gallery',
+				'post_type'   => 'modula-gallery',
 				'post_status' => 'publish',
 			);
 
@@ -307,49 +310,49 @@ class Modula_Upgrades {
 			}
 
 			// In modula 2.0 the hoverEffect it's renamed to effect.
-			$modula_settings[ 'effect' ] = $modula_settings['hoverEffect'];
+			$modula_settings['effect'] = $modula_settings['hoverEffect'];
 			unset( $modula_settings['hoverEffect'] );
 
-			$modula_settings[ 'enableSocial' ] = (isset($modula_settings['disableSocial']) && '1' == $modula_settings['disableSocial']) ? 0 : 1;
+			$modula_settings['enableSocial'] = ( isset( $modula_settings['disableSocial'] ) && '1' == $modula_settings['disableSocial'] ) ? 0 : 1;
 			unset( $modula_settings['disableSocial'] );
 
-            $default_gallery_settings = array(
-                'type'                      => 'creative-gallery',
-                'width'                     => '100%',
-                'height'                    => '800',
-                'img_size'                  => 300,
-                'margin'                    => '10',
-                'randomFactor'              => '50',
-                'lightbox'                  => 'fancybox',
-                'shuffle'                   => 0,
-                'captionColor'              => '#ffffff',
-                'hide_title'                => 1,
-                'hide_description'          => 0,
-                'captionFontSize'           => '14',
-                'titleFontSize'             => '16',
-                'enableFacebook'            => 0,
-                'enablePinterest'           => 0,
-				'enableTwitter'             => 0,
-				'enableWhatsapp'            => 0,
-				'enableEmail'               => 0,
-                'filterClick'               => 0,
-				'socialIconColor'           => '#ffffff',
-				'socialIconSize'            => 16,
-				'socialIconPadding'         => 20,
-                'loadedScale'               => '100',
-                'inView'                    => '100',
-                'effect'                    => 'pufrobo',
-                'borderColor'               => '#ffffff',
-                'borderRadius'              => '0',
-                'borderSize'                => '0',
-                'shadowColor'               => '#ffffff',
-                'shadowSize'                => 0,
-                'script'                    => '',
-                'style'                     => '',
-                'columns'                   => 6,
-                'gutter'                    => 10,
-                'helpergrid'                => 0,
-            );
+			$default_gallery_settings = array(
+				'type'              => 'creative-gallery',
+				'width'             => '100%',
+				'height'            => '800',
+				'img_size'          => 300,
+				'margin'            => '10',
+				'randomFactor'      => '50',
+				'lightbox'          => 'fancybox',
+				'shuffle'           => 0,
+				'captionColor'      => '#ffffff',
+				'hide_title'        => 1,
+				'hide_description'  => 0,
+				'captionFontSize'   => '14',
+				'titleFontSize'     => '16',
+				'enableFacebook'    => 0,
+				'enablePinterest'   => 0,
+				'enableTwitter'     => 0,
+				'enableWhatsapp'    => 0,
+				'enableEmail'       => 0,
+				'filterClick'       => 0,
+				'socialIconColor'   => '#ffffff',
+				'socialIconSize'    => 16,
+				'socialIconPadding' => 20,
+				'loadedScale'       => '100',
+				'inView'            => '100',
+				'effect'            => 'pufrobo',
+				'borderColor'       => '#ffffff',
+				'borderRadius'      => '0',
+				'borderSize'        => '0',
+				'shadowColor'       => '#ffffff',
+				'shadowSize'        => 0,
+				'script'            => '',
+				'style'             => '',
+				'columns'           => 6,
+				'gutter'            => 10,
+				'helpergrid'        => 0,
+			);
 
 			if ( isset( $modula_settings['filters'] ) ) {
 				$modula_settings['filters'] = explode( '|', $modula_settings['filters'] );
@@ -364,7 +367,7 @@ class Modula_Upgrades {
 			require_once MODULA_PATH . 'includes/admin/class-modula-image.php';
 
 			$img_size = absint( $modula_settings['img_size'] );
-			$resizer = new Modula_Image();
+			$resizer  = new Modula_Image();
 
 			foreach ( $images as $image ) {
 
@@ -374,14 +377,14 @@ class Modula_Upgrades {
 				}
 
 				$new_images[] = array(
-					'id'          => absint($image['imageId']),
+					'id'          => absint( $image['imageId'] ),
 					'alt'         => '',
-					'title'       => sanitize_text_field($image['title']),
+					'title'       => sanitize_text_field( $image['title'] ),
 					'description' => $image['description'],
-					'halign'      => sanitize_text_field($image['halign']),
-					'valign'      => sanitize_text_field($image['valign']),
-					'link'        => esc_url_raw($image['link']),
-					'target'      => '_blank' == $image['target'] ? 1: 0,
+					'halign'      => sanitize_text_field( $image['halign'] ),
+					'valign'      => sanitize_text_field( $image['valign'] ),
+					'link'        => esc_url_raw( $image['link'] ),
+					'target'      => '_blank' == $image['target'] ? 1 : 0,
 					'filters'     => str_replace( '|', ',', $image['filters'] ),
 				);
 
@@ -396,7 +399,7 @@ class Modula_Upgrades {
 			) );
 			die;
 
-		}else{
+		} else {
 
 			echo json_encode( array(
 				'status'  => 'succes',
@@ -413,7 +416,7 @@ class Modula_Upgrades {
 		// Run a security check first.
 		check_admin_referer( 'modula-upgrade-complete-nonce', 'nonce' );
 
-		$completed = get_option( $this->upgrades_key, array() );
+		$completed              = get_option( $this->upgrades_key, array() );
 		$completed['modula_v2'] = true;
 		update_option( $this->upgrades_key, $completed );
 
@@ -425,7 +428,7 @@ class Modula_Upgrades {
 
 	}
 
-	public function dismiss_notice(){
+	public function dismiss_notice() {
 
 		check_admin_referer( 'modula-dismiss-upgrade', 'nonce' );
 
@@ -434,8 +437,8 @@ class Modula_Upgrades {
 			die();
 		}
 
-		$upgrades = get_option( $this->upgrades_key, array() );
-		$upgrades_key = sanitize_text_field( $_POST['key'] );
+		$upgrades                   = get_option( $this->upgrades_key, array() );
+		$upgrades_key               = sanitize_text_field( $_POST['key'] );
 		$completed[ $upgrades_key ] = true;
 		update_option( $this->upgrades_key, $completed );
 
@@ -444,34 +447,34 @@ class Modula_Upgrades {
 
 	}
 
-	public function dismiss_notices_script(){
+	public function dismiss_notices_script() {
 		if ( $this->isNotice ) {
 			?>
-			<script type="text/javascript">
-				jQuery(document).ready(function($){
-					$('.modula-upgrade-notice .notice-dismiss').click(function( evt ){
-						evt.preventDefault();
+            <script type="text/javascript">
+                jQuery(document).ready(function ($) {
+                    $('.modula-upgrade-notice .notice-dismiss').click(function (evt) {
+                        evt.preventDefault();
 
-						var key = $(this).data('key');
-						$.ajax({
-							url:      "<?php echo esc_url( admin_url( 'admin-ajax.php' ) ) ?>",
-				            type:     'POST',
-				            dataType: 'json',
-				            data: {
-				                action: 'modula-dismiss-upgrade',
-				                key: key,
-				                nonce:  '<?php echo wp_create_nonce( 'modula-dismiss-upgrade' ) ?>',
-				            },
-				            success: function( response ) {
-				            	if ( response.success ) {
-				            		location.reload();
-				            	}
-				            }
-						});
+                        var key = $(this).data('key');
+                        $.ajax({
+                            url: "<?php echo esc_url( admin_url( 'admin-ajax.php' ) ) ?>",
+                            type: 'POST',
+                            dataType: 'json',
+                            data: {
+                                action: 'modula-dismiss-upgrade',
+                                key: key,
+                                nonce: '<?php echo wp_create_nonce( 'modula-dismiss-upgrade' ) ?>',
+                            },
+                            success: function (response) {
+                                if (response.success) {
+                                    location.reload();
+                                }
+                            }
+                        });
 
-					});
-				});
-			</script>
+                    });
+                });
+            </script>
 			<?php
 		}
 	}
