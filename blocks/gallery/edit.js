73824e1160b35dd651bcf7e2487a40834ebda0cd
Cosmin Cristea <cristea.cosmin87@gmail.com>
2023-10-12 14:48:48 +0300

    Added prettier and prettier config

--- blocks/gallery/edit.js
+++ blocks/gallery/edit.js
@@ -1,7 +1,11 @@
 import './editor.scss';
 
 export function Edit(props) {
-	return <div className="modula-block-preview">Hello world - it's going to be ok</div>;
+	return (
+		<div className="modula-block-preview">
+			Hello world - it's going to be ok
+		</div>
+	);
 }
 
 export default Edit;
