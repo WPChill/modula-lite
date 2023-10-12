14405b801fc4228240f4dcb776b1e39c2704bb48
Cosmin Cristea <cristea.cosmin87@gmail.com>
2023-10-12 14:41:00 +0300

    ma doare capul

--- build/gallery/index.asset.php
+++ build/gallery/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '00919b95ef23a6b42a6d');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => '5a4c251d4866d12ef5e6');
