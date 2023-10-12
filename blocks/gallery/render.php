88138627f6a9f8f057159c59ed7fa50d5b47ee9e
Cosmin Cristea <cristea.cosmin87@gmail.com>
2023-10-12 09:28:39 +0300

    updated render function

--- blocks/gallery/render.php
+++ blocks/gallery/render.php
@@ -1,8 +1,23 @@
 <?php
+
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 ?>
 <p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Modula Gallery – hello from a dynamic block!', 'gallery' ); ?>
+	<?php
+	if (!isset($attributes['id'])) {
+		return;
+	}
+	if (!isset($attributes['align'])) {
+		$atts['align'] = '';
+	}
+
+	if (isset($attributes['galleryType']) && 'gallery' !== $attributes['galleryType']) {
+		$html = apply_filters('modula_render_defaults_block', 'An error occurred', $attributes);
+		echo $html;
+	} else {
+		echo '[modula id=' . absint($attributes['id']) . ' align=' . esc_attr($attributes['align']) . ']';
+	}
+	?>
 </p>
