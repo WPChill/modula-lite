d692bfc4a3a582335fbf5311a8141914a11b72eb
TeoAlex <teoalexsin@gmail.com>
2023-12-22 16:22:00 +0200

    [ #894 ] fancybox 5 update

--- includes/public/class-modula-shortcode.php
+++ includes/public/class-modula-shortcode.php
@@ -388,30 +388,11 @@ class Modula_Shortcode {
 	 */
 	public function fancybox_options($settings){
 
-		$fancybox_options = Modula_Helper::lightbox_default_options();
-
-		if ( isset( $settings['show_navigation'] ) && '1' == $settings['show_navigation'] ) {
-			$fancybox_options['arrows'] = true;
-		}
-
-		$fancybox_options['baseTpl'] = '<div class="modula-fancybox-container modula-lightbox-' . $settings['gallery_id'] . '" role="dialog" tabindex="-1">' .
-		                               '<div class="modula-fancybox-bg"></div>' .
-		                               '<div class="modula-fancybox-inner">' .
-		                               '<div class="modula-fancybox-infobar"><span data-fancybox-index></span>&nbsp;/&nbsp;<span data-fancybox-count></span></div>' .
-		                               '<div class="modula-fancybox-toolbar">{{buttons}}</div>' .
-		                               '<div class="modula-fancybox-navigation">{{arrows}}</div>' .
-		                               '<div class="modula-fancybox-stage"></div>' .
-		                               '<div class="modula-fancybox-caption"><div class="modula-fancybox-caption__body"></div></div>' .
-		                               "</div>" .
-		                               "</div>";
-
 		/**
 		 * Hook: modula_fancybox_options.
 		 *
 		 */
-		$fancybox_options = apply_filters( 'modula_fancybox_options', $fancybox_options, $settings );
-
-		return $fancybox_options;
+		return apply_filters( 'modula_fancybox_options', Modula_Helper::lightbox_default_options(), $settings );
 	}
 
 
