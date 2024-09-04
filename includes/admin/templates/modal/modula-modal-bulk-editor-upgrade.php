987dfb295e93f81b5be2924ae12ad5debefc4878
Razvan Aldea <44024114+razvanaldea89@users.noreply.github.com>
2024-09-04 11:56:37 +0300

    wpcs

--- includes/admin/templates/modal/modula-modal-bulk-editor-upgrade.php
+++ includes/admin/templates/modal/modula-modal-bulk-editor-upgrade.php
@@ -15,15 +15,37 @@
 			<div class="modula-upsells-carousel-wrapper-modal">
 				<div class="modula-upsells-carousel-modal">
 					<div class="modula-upsell-modal modula-upsell-item-modal">
-						<h2><?php
-							esc_html_e( 'Bulk Editor', 'modula-best-grid-gallery' ); ?></h2>
-						<h4 class="modula-upsell-description-modal"><?php
-							esc_html_e( 'Upgrade to a Pro plan and start editing multiple images from your gallery at the same time.', 'modula-best-grid-gallery' ); ?></h4>
+						<h2>
+							<?php
+							esc_html_e( 'Bulk Editor', 'modula-best-grid-gallery' );
+							?>
+						</h2>
+						<h4 class="modula-upsell-description-modal">
+							<?php
+							esc_html_e( 'Upgrade to a Pro plan and start editing multiple images from your gallery at the same time.', 'modula-best-grid-gallery' );
+							?>
+						</h4>
 						<ul class="modula-upsells-list-modal">
-							<li><?php esc_html_e( 'Reduce the time required to set your gallery images information.', 'modula-best-grid-gallery' ); ?></li>
-							<li><?php esc_html_e( 'You can edit the title, caption, alt text of your images.', 'modula-best-grid-gallery' ); ?></li>
-							<li><?php esc_html_e( 'Set the filters for your images.', 'modula-best-grid-gallery' ); ?></li>
-							<li><?php esc_html_e( 'Premium support.', 'modula-best-grid-gallery' ); ?></li>
+							<li>
+								<?php
+								esc_html_e( 'Reduce the time required to set your gallery images information.', 'modula-best-grid-gallery' );
+								?>
+							</li>
+							<li>
+								<?php
+								esc_html_e( 'You can edit the title, caption, alt text of your images.', 'modula-best-grid-gallery' );
+								?>
+							</li>
+							<li>
+								<?php
+								esc_html_e( 'Set the filters for your images.', 'modula-best-grid-gallery' );
+								?>
+							</li>
+							<li>
+								<?php
+								esc_html_e( 'Premium support.', 'modula-best-grid-gallery' );
+								?>
+							</li>
 						</ul>
 						<p>
 							<?php
@@ -32,7 +54,7 @@
 							$buttons         = '<a target="_blank" href="' . esc_url( $lite_vs_pro_url ) . '"  class="button">' . esc_html__( 'Free vs Premium', 'modula-best-grid-gallery' ) . '</a>';
 							$buttons         .= '<a target="_blank" href="https://wp-modula.com/pricing/?utm_source=upsell&utm_medium=popup&utm_campaign=bulk-editor" style="margin-top:10px;" class="button-primary button">' . esc_html__( 'Get Premium!', 'modula-best-grid-gallery' ) . '</a>';
 
-							echo apply_filters( 'modula_upsell_buttons', $buttons, 'bulk_editor' );
+							echo wp_kses_post( apply_filters( 'modula_upsell_buttons', $buttons, 'bulk_editor' ) );
 
 							?>
 						</p>
