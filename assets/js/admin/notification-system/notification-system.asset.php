005432494ee19148fb68ff0d5820a1502b366f73
Cosmin Cristea <cristea.cosmin87@gmail.com>
2024-11-27 11:07:40 +0200

    Updated notification system

--- assets/js/admin/notification-system/notification-system.asset.php
+++ assets/js/admin/notification-system/notification-system.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b0bbe8c9530d68a47af1');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e9b0768b4f909a8b9e65');
