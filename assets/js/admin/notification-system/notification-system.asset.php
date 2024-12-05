33699b32124c083ce095f0922e5456a5835f9e16
TeoAlex <teoalexsin@gmail.com>
2024-12-05 12:52:51 +0200

    build

--- assets/js/admin/notification-system/notification-system.asset.php
+++ assets/js/admin/notification-system/notification-system.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2713718bd805816fe37f');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c106a42061a7c2230c5c');
