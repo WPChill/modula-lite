abc8f8f42f65765842eca9ef79a788296fe57174
Cosmin Cristea <cristea.cosmin87@gmail.com>
2025-02-19 11:51:04 +0200

    Add AI-powered image description and metadata generation features

--- assets/js/admin/settings/index.asset.php
+++ assets/js/admin/settings/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '26f9c12e7b72d7a296a6');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2609614bd87cc7d183e0');
